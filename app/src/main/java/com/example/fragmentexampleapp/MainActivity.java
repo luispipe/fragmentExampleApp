dc0324c0d87a14903ba3966f128f09bf4a491117
luispipe <lflondonor@unal.edu.co>
2023-08-03 12:04:22 -0300

    factorial commit

--- app/src/main/java/com/example/fragmentexampleapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentexampleapp/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.fragmentexampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_home);
     }
 }
